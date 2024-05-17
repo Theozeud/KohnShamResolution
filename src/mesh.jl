17372aa03fdcf048c00ce5ad13730a883504a069
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2024-05-17 19:23:49 +0200

    update mesh

--- src/mesh.jl
+++ src/mesh.jl
@@ -1,14 +1,14 @@
 abstract type AbstractMesh end
 
-struct 1DMesh{TP,TS} <: AbstractMesh
-    points
-    steps
+struct OneDMesh{TP,TS} <: AbstractMesh
+    points::TP
+    steps::TS
 end
 
 function mesh(points::AbstractArray{T} where T<: Real)
     _points = union(sort(points))
-    steps = [_points[i+1] -  _points[i] for i in eachindex(_points)[1:end-1]]
-    1Dmesh(_points , steps)
+    steps = [_points[i+1] - _points[i] for i in eachindex(_points)[1:end-1]]
+    OneDmesh(_points , steps)
 end
 
 function mesh(point::Real, fun::Base.Callable, N::Int)
@@ -20,12 +20,12 @@ function mesh(point::Real, fun::Base.Callable, N::Int)
     mesh(points)
 end
 
-@inline Base.length(m::1DMesh) = length(m.points)
-@inline Base.size(m::1DMesh) = size(m.points)
-@inline points(m::1DMesh) = m.points
-@inline steps(m::1DMesh) = m.steps
-@inline Base.getindex(m::1DMesh, n::Int) = m.points[n]
-@inline Base.iterate(m::1DMesh, state = 1) = state > size(m) ? nothing : (m[state],state+1)
-@inline left(m::1DMesh) = m[1]
-@inline right(m::1DMesh) = m[end]
-@inline edges(m::1DMesh) = (m[1],m[end])
+@inline Base.length(m::OneDMesh) = length(m.points)
+@inline Base.size(m::OneDMesh) = size(m.points)
+@inline points(m::OneDMesh) = m.points
+@inline steps(m::OneDMesh) = m.steps
+@inline Base.getindex(m::OneDMesh, n::Int) = m.points[n]
+@inline Base.iterate(m::OneDMesh, state = 1) = state > size(m) ? nothing : (m[state],state+1)
+@inline left(m::OneDMesh) = m[1]
+@inline right(m::OneDMesh) = m[end]
+@inline edges(m::OneDMesh) = (m[1],m[end])
