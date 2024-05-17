907e2ad074a5f74eed53e9fecbd8922245e82bb3
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2024-05-17 18:04:40 +0200

    Add Mesh

---
+++ src/mesh.jl
@@ -0,0 +1,30 @@
+abstract type AbstractMesh end
+
+struct 1DMesh{TP,TS} <: AbstractMesh
+    points
+    steps
+end
+
+function mesh(points::AbstractArray{T} where T<: Real)
+    _points = union(sort(points))
+    steps = [_points[i+1] -  _points[i] for i in eachindex(_points)[1:end-1]]
+    1Dmesh(_points , steps)
+end
+
+function mesh(point::Real, fun::Base.Callable, N::Int)
+    points = zeros(N)
+    points[1] = point
+    for i in 2:N
+        @inbounds points[i] = fun(points[i-1])
+    end
+    mesh(points)
+end
+
+@inline Base.length(m::1DMesh) = length(m.points)
+@inline Base.size(m::1DMesh) = size(m.points)
+@inline points(m::1DMesh) = m.points
+@inline steps(m::1DMesh) = m.steps
+@inline left(m::1DMesh) = m.points[1]
+@inline right(m::1DMesh) = m.points[end]
+@inline Base.getindex(m::1DMesh, n::Int) = m.points[n]
+@inline Base.iterate(m::1DMesh, state = 1) = state > size(m) ? nothing : (m[state],state+1)
