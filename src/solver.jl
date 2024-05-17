56bd6185c7f95edca11a571e9f234daeb8e8cfaf
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2024-05-17 19:23:57 +0200

    add solver

---
+++ src/solver.jl
@@ -0,0 +1,16 @@
+struct SolverOptions{T,Tr,Teps}
+    lh::T      # Cut-off sections of the decomposition in eigenspace
+    Rmax::Tr    # Cut-off of the radial domain
+    Nr::T
+    Ngr::T
+    Ngθ::T
+    ϵ::Teps      
+end
+
+
+struct Solver{}
+    mesh::AbstractMesh
+    sol
+    opts::SolverOptions
+
+end
