88357f9588b491e80988ebb198d5dbddb6bf3283
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2024-05-17 18:59:41 +0200

    Update model.jl

--- src/model.jl
+++ src/model.jl
@@ -3,13 +3,15 @@ abstract type AbstractDFTModel end
 ```
     KohnSham Model
 ```
-struct KohnShamExtended{TZ,TN,TEXCH} <: AbstractDFTModel
+struct KohnShamExtended{TZ,TN,TEXCH,TPOT} <: AbstractDFTModel
     z::TZ
     N::TN
     exc::TEXCH
+    potential::TPOT
 end
 
 Base.size(km::KohnShamExtended) = (km.z,km.N)
 @inline exchcorr(km::KohnShamExtended) = km.exc
 @inline charge(km::KohnShamExtended) = km.z
 @inline nbelec(km::KohnShamExtended) = km.N
+@inline potential(km::KohnShamExtended) = km.potential
