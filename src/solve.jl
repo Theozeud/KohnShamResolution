90bc8cac9ab75a66bef30e4036d5e1a0292d7954
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2024-05-17 19:24:10 +0200

    add idea solve

---
+++ src/solve.jl
@@ -0,0 +1,26 @@
+function groundstate(model::AbstractDFTModel; kwargs...)
+    solver = init(model; kwargs...)
+    solve!(solver; kwargs...)
+    makesolution()
+end
+
+
+function init(model::AbstractDFTModel; kwargs...)
+    # Init mesh
+    # Init matrix
+    # Init Initial condition
+end
+
+
+function solve(solver::KhonShamSolver; kwargs...)
+    while #stopping criteria
+        loopheader()
+        performstep!()
+        loopfooter()
+    end
+end
+
+
+function makesolution()
+
+end
