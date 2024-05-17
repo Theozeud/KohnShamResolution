ea4fb597b6994998880d645d799338da04b94768
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2024-05-17 19:24:21 +0200

    add empty files

---
+++ src/performstep.jl
@@ -0,0 +1,13 @@
+function performstep!()
+    # step 1 : find potential 
+    
+    # step 2 : compute an approximation of the exchange correlation term
+
+    # step 3 : résolution du problème aux valeurs propres blocs par blocs
+
+    # step 4 : building a certain matrix
+
+    # step 5 : update this matrix with a convex approach
+
+    # step 6 : stopping criteria
+end
