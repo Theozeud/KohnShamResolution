97f9fc80536429e78c10f1a19d8258d84a2bea83
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2024-06-10 16:24:15 +0200

    first small implementation of Solution Structure

--- src/solution.jl
+++ src/solution.jl
@@ -1,6 +1,36 @@
 abstract type AbstractKohnShamSolution end
 
-struct KohnShamSolution 
-    solver
+struct KohnShamSolution
+    success::String 
+    ϵ
+    n
+    niter::Int
+    crit::Vector
 
+    function KohnShamSolution(solver::KhonShamSolver)
+        if solver.niter == solver.opts.maxiter
+            success = "MAXITERS"
+        else
+            success = "SUCCESS"
+        end
+        new(success, solver.ϵ , solver.n, solver.niter, solver.values_stop_crit)
+        
+    end
+end
+
+function Base.show(io::IO, sol::KohnShamSolution)
+    printstyled(io, "Sucess = "; bold = true)
+    if sol.success == "SUCCESS"
+        printstyled(io, string(sol.success)*"\n"; bold = true, color = :green)
+    else
+        printstyled(io, string(sol.success)*"\n"; bold = true, color = :red)
+    end
+    printstyled(io, "ϵ = "; bold = true)
+    println(io, string(sol.ϵ))
+    printstyled(io, "n = "; bold = true)
+    println(io, string(sol.n))
+    printstyled(io, "niter = "; bold = true)
+    println(io, string(sol.niter))
+    printstyled(io, "Stopping criteria = "; bold = true)
+    println(io, string(last(sol.crit)))
 end
