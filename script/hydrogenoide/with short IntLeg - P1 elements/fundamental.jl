98976eb6c7075e87510440cc9036224fd1bfad51
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2024-06-28 23:20:09 +0200

    test & image intlegendre + p1

--- script/hydrogenoide/with short IntLeg - P1 elements/fundamental.jl
+++ script/hydrogenoide/with short IntLeg - P1 elements/fundamental.jl
@@ -1,11 +1,6 @@
 using KohnShamResolution
 using Plots
 
-# One electron model
-zA = [1, 4, 8, 16]
-N = 1
-eigvalue_theo(n,z) = -z^2/(2*n^2)
-
 # Choice of the method
 method = ConstantODA(1.0)
 
@@ -18,8 +13,16 @@ T = Float64
 ordermin = 2
 ordermax = 4
 
-# Plots
+# One electron model
+zA = [1, 2, 3, 4]
+N = 1
+eigvalue_theo(n,z)    = -z^2/(2*n^2)
+fundamental(z, x)     = z^(3/2)/sqrt(π)*exp(-z*abs(x))
+
+# Plot
 pltA = []
+
+cutting_pre = 5
 
 for z in zA
 
@@ -29,11 +32,13 @@ for z in zA
     p1 = ShortP1Basis(m, T;  normalize = true, left = false, right = false)
     intleg = ShortIntLegendreBasis(m, T; normalize = true, ordermin = ordermin, ordermax = ordermax)
     basis = CombineShortPolynomialBasis(p1, intleg)
-
     D = KohnShamSphericalDiscretization(lₕ, basis, m)
+
     KM = KohnShamExtended(z = z, N = N)
 
-    sol = groundstate(KM, D, method; tol = 1e-4, hartree = false)
+    sol = groundstate(KM, D, method; tol = 1e-20, hartree = false, maxiter = 1)
+
+    fun = sol.eigvects[1]
 
     plt = plot( size = (900,600), margin = 0.5Plots.cm, legend = :bottomright,
                 legendfontsize  = 14,  
@@ -41,31 +46,18 @@ for z in zA
                 guidefontsize   = 14,
                 tickfontsize    = 14)
 
-    xlabel!("n")
-    ylabel!("Energie")
+    xlabel!("r")
+    ylabel!("Fundamental")
     title!("z = "*string(z))
 
-    index_ϵ = findall(x->x < 0, sol.ϵ)
+    X = logmesh(Rmin, Rmax,1000).points
 
-    scatter!(index_ϵ, eigvalue_theo.(index_ϵ,z),  label = "Théorique",
-                markershape = :circ, 
-                markersize = 8,
-                markeralpha = nothing,
-                markercolor = :red,
-                markerstrokewidth = 1,
-                markerstrokecolor = :red
-                )
+    plot!(X, fundamental.(z,X),  label = "Théorique", color = :red, lw = 3)
 
-    scatter!(index_ϵ, sol.ϵ[index_ϵ],  label = "Numérique",
-                markershape = :x, 
-                markersize = 10,
-                markeralpha = nothing,
-                markercolor = :black,
-                markerstrokewidth = 2)
+    plot!(plt, X, -fun.(X), label = "Numérique", color = :blue)
 
     push!(pltA, plt)
-
 end
 
 pltfin = plot(pltA..., layout = (2,2), size = (1200,1000))
-savefig(pltfin, "image/hydrogenoide/with short IntLeg elements/Valeurs propres avec Nmesh = "*string(Nmesh)*" et ordres = "*string(ordermax))
+savefig(pltfin, "image/hydrogenoide/with short IntLeg elements/Vecteurs propres avec Nmesh = "*string(Nmesh)*" et ordres = "*string(ordermax))
