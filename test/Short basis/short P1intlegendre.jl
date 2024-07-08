cefac49b8d8579c1275f3c92dc7e224444487be5
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2024-07-08 17:00:21 +0200

    Update short P1intlegendre.jl

--- test/Short basis/short P1intlegendre.jl
+++ test/Short basis/short P1intlegendre.jl
@@ -6,7 +6,7 @@ T = Float64
 Rmin = 0
 Rmax = 5
 Nmesh = 6
-m = linmesh(Rmin,Rmax,Nmesh)
+m = logmesh(Rmin,Rmax,Nmesh)
 normalize = true
 ordermin = 2
 ordermax = 5
@@ -18,7 +18,7 @@ basis = ShortP1IntLegendreBasis(m, T; ordermin = ordermin, ordermax = ordermax, 
 
 # Plots elements of the basis
 X = LinRange(-1, 1, 1000)
-plt_elements = plot()
+plt_elements = plot(legend = false)
 for b ∈ basis.basisVector
     for i ∈ eachindex(b.elements)
         plot!(plt_elements, X, b.elements[i].(X))
@@ -29,7 +29,7 @@ plt_elements
 
 # Plots of basis
 X = LinRange(Rmin, Rmax, Nmesh * 100)
-plt_basis = plot()
+plt_basis = plot(legend = false)
 for i ∈ 1:length(basis)
     p = build_basis(basis, i)
     plot!(plt_basis, X, p.(X))
@@ -40,7 +40,7 @@ plt_basis
 # Plots of the derivatives of the basis
 deriv_basis = deriv(basis)
 X = LinRange(Rmin, Rmax, Nmesh * 100)
-plt_derivbasis = plot()
+plt_derivbasis = plot(legend = false)
 for i ∈ 1:length(deriv_basis)
     p = build_basis(deriv_basis, i)
     plot!(plt_derivbasis, X, p.(X))
@@ -51,7 +51,7 @@ plt_derivbasis
 # Assembly matrix
 M₀  = mass_matrix(basis)
 M₋₁ = weight_mass_matrix(basis, -1)
-#M₋₂ = weight_mass_matrix(basis, -2)
+M₋₂ = weight_mass_matrix(basis, -2)
 A   = mass_matrix(deriv(basis))
 
 
