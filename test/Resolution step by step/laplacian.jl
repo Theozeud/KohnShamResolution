31d12a964c408bbf1aaf10dd1fa25ce1dc34074f
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2024-07-12 14:04:53 +0200

    more careful for type

--- test/Resolution step by step/laplacian.jl
+++ test/Resolution step by step/laplacian.jl
@@ -1,28 +1,25 @@
 using KohnShamResolution
 using Plots
 using LinearAlgebra
-using GenericLinearAlgebra
-using Quadmath
 
-# General Discretization Parameters
-T = Float128
-Rmin = zero(T)
+using GenericLinearAlgebra
+using DoubleFloats
 
 # Theoretical Eigenvalue and Eigenvectors
-vect_theo(n, Rmax) = x-> sqrt(T(2)/T(Rmax)) * sin(n*T(π)*x/T(Rmax)) 
-val_theo(n, Rmax)  = (T(π)/T(Rmax))^2 * n^2
+vect_theo(n, Rmax, T) = x-> sqrt(T(2)/T(Rmax)) * sin(n*T(π)*x/T(Rmax)) 
+val_theo(n, Rmax, T)  = (T(π)/T(Rmax))^2 * n^2
 
 ##############################################################################
 # Function to compute eigenvalue and eigenvector for different discretizations
 
 # With finite difference
-function eigen_with_finite_diff(Nmesh, Rmax)
-    A = SymTridiagonal(2*ones(T, Nmesh), -ones(T, Nmesh-1)) .* ((Nmesh - 1)/ T(Rmax))^2
+function eigen_with_finite_diff(Nmesh, Rmax, T)
+    A = SymTridiagonal(2*ones(T, Nmesh), - ones(T, Nmesh-1)) .* ((T(Nmesh) - T(1))/ T(Rmax))^2
     eigvals(A)
 end
 
 # With P1
-function eigen_with_P1(mesh)
+function eigen_with_P1(mesh, T)
     normalize = true
     left = false
     right = false
@@ -33,7 +30,7 @@ function eigen_with_P1(mesh)
 end
 
 # With P1-Integrated Legendre Polynomials
-function eigen_with_P1IntLeg(mesh, ordermax)
+function eigen_with_P1IntLeg(mesh, ordermax, T)
     normalize = true
     ordermin = 2
     left = false
@@ -45,47 +42,47 @@ function eigen_with_P1IntLeg(mesh, ordermax)
 end
 
 # With P1
-function eigen_with_P1(mesh, n)
+function eigen_with_P1(mesh, n, T)
     normalize = true
     left = false
     right = false
-    Rmax = mesh[end]
+    Rmax = T(last(mesh))
     basis = ShortP1Basis(mesh, T; left = left, right = right, normalize = normalize)
     M₀  = Symmetric(mass_matrix(basis))
     AP1   = Symmetric(mass_matrix(deriv(basis)))
     eign = build_on_basis(basis, eigen(AP1, M₀).vectors[:,n])
-    eign/ sqrt(integrate(eign*eign, Rmin, Rmax))
+    eign / sqrt(integrate(eign*eign, T(Rmin), T(Rmax)))
 end
 
 # With P1-Integrated Legendre Polynomials
-function eigen_with_P1IntLeg(mesh, ordermax, n)
+function eigen_with_P1IntLeg(mesh, ordermax, n, T)
     normalize = true
     ordermin = 2
     left = false
     right = false
-    Rmax = last(mesh)
+    Rmax = T(last(mesh))
     basis = ShortP1IntLegendreBasis(mesh, T; ordermin = ordermin, ordermax = ordermax,  normalize = normalize, left = left, right = right)
     M₀  = Symmetric(mass_matrix(basis))
     AIntleg2   = Symmetric(mass_matrix(deriv(basis)))
     eign = build_on_basis(basis, eigen(AIntleg2, M₀).vectors[:,n])
-    eign/ sqrt(integrate(eign*eign, Rmin, Rmax))
+    eign/ sqrt(integrate(eign*eign, T(Rmin), T(Rmax)))
 end
 
 ##############################################################################
 # Plot Eigenvalue
 
-function plot_eigenvalue(Nmesh, Rmax, order = [2,3])
+function plot_eigenvalue(Nmesh, Rmax, order = [2,3], T)
     # Creation of the mesh
-    m = linmesh(Rmin, Rmax, Nmesh)
+    m = linmesh(Rmin, Rmax, Nmesh, T)
     
     # With finite difference
-    #vals_diff = eigen_with_finite_diff(Nmesh, Rmax)
+    #vals_diff = eigen_with_finite_diff(Nmesh, Rmax, T)
     # With P1
-    vals_p1 = eigen_with_P1(m)
+    vals_p1 = eigen_with_P1(m, T)
     # With P1-Integrated Legendre Polynomials
     vals_il = []
     for o ∈ order
-        vals_ilo = eigen_with_P1IntLeg(m, o)
+        vals_ilo = eigen_with_P1IntLeg(m, o, T)
         push!(vals_il, vals_ilo)
     end
 
@@ -100,7 +97,7 @@ function plot_eigenvalue(Nmesh, Rmax, order = [2,3])
         plot!(vals_il[i], label = "P1 + Integrated Legendre ordre "*string(o), lw = 4)
         max_index = max(length(vals_p1), length(vals_il[i]))
     end
-    plot!(val_theo.(1:max_index,Rmax), label = "Theoretical", lw = 4)
+    plot!(val_theo.(1:max_index, Rmax), label = "Theoretical", lw = 4)
     return plt_eigenvalue_intleg
 end
 
@@ -109,17 +106,17 @@ end
 
 function plot_eigenvector(Nmesh, Rmax, n, order)
     # Creation of the mesh
-    m = linmesh(Rmin, Rmax, Nmesh)
+    m = linmesh(Rmin, Rmax, Nmesh, T)
    
     # With finite difference
     #_, eigs_diff = eigen_with_finite_diff(Nmesh, Rmax)
+    # plot!(plt_eigenvector, range(Rmin, Rmax, Nmesh), eigs_diff[:,n]/ sqrt((Rmax - Rmin)/(Nmesh-1)), label = "Finite Difference", lw = 2)
     # With P1
     #eigs_p1 = eigen_with_P1(m, n)
-    # With P1-Integrated Legendre Polynomials
-    #plot!(plt_eigenvector, range(Rmin, Rmax, Nmesh), eigs_diff[:,n]/ sqrt((Rmax - Rmin)/(Nmesh-1)), label = "Finite Difference", lw = 2)
-    #plot!(plt_eigenvector, X, (sign(vect_theo(n, Rmax)(1) * eigs_p1(1)) * eigs_p1).(X), label = "P1", lw = 2)
-
-    eigs_il = eigen_with_P1IntLeg(m, order, n)
+    # plot!(plt_eigenvector, X, (sign(vect_theo(n, Rmax)(1) * eigs_p1(1)) * eigs_p1).(X), label = "P1", lw = 2)
+       
+    # With Integrated Legendre 
+    eigs_il = eigen_with_P1IntLeg(m, order, n, T)
     
     X = LinRange(Rmin, Rmax, 10000)
     plt_eigenvector = plot( size= (1500, 700), margin = 1Plots.cm,                
@@ -138,32 +135,32 @@ end
 ##################################################################################
 # Compute Error
 
-function compute_error_eigenvalue(vecNmesh, Rmax, num)
+function compute_error_eigenvalue(vecNmesh, Rmax, num, T)
 
     label = ["Différence finie", "P1", "IntLeg 2", "IntLeg 3", "IntLeg 4"]
     ϵerror = zeros(T, length(vecNmesh), length(label))
     for (i, Nmesh) ∈ enumerate(vecNmesh)
         # Creation of the mesh
-        m = linmesh(Rmin, Rmax, Nmesh)
+        m = linmesh(Rmin, Rmax, Nmesh, T)
         
         # True eigenvalue
         @time "Nmesh = "*string(Nmesh) begin
-        true_val = val_theo.(T.(1:Nmesh), T(Rmax))
+        true_val = val_theo.(1:Nmesh, Rmax, T)
 
         # With finite difference
         vals_diff = eigen_with_finite_diff(Nmesh, Rmax)
 
         # With P1
-        vals_p1 = eigen_with_P1(m)
+        vals_p1 = eigen_with_P1(m, T)
 
         # With P1-Integrated Legendre Polynomials ordre 2
-        vals_il2 = eigen_with_P1IntLeg(m, 2)
+        vals_il2 = eigen_with_P1IntLeg(m, 2, T)
 
         # With P1-Integrated Legendre Polynomials ordre 3
-        vals_il3 = eigen_with_P1IntLeg(m, 3)
+        vals_il3 = eigen_with_P1IntLeg(m, 3, T)
 
         # With P1-Integrated Legendre Polynomials ordre 4
-        vals_il4 = eigen_with_P1IntLeg(m, 4)
+        vals_il4 = eigen_with_P1IntLeg(m, 4, T)
         end
 
         # Compute the error for eigenvalues and the fundamental
@@ -181,16 +178,22 @@ function compute_error_eigenvalue(vecNmesh, Rmax, num)
                         guidefontsize   = 14,
                         tickfontsize    = 14)
     xlabel!(plt_ϵ_error, "Nmesh")
-    ylabel!(plt_ϵ_error, "Error on the "*string(num)*"-th eigenvalue")
-    title!(plt_ϵ_error, "Rmax = "*string(Rmax))
+    ylabel!(plt_ϵ_error, "Error on the $num-th eigenvalue")
+    title!(plt_ϵ_error, "Rmax = $Rmax")
     for i ∈ eachindex(label)
         plot!(plt_ϵ_error, vecNmesh, ϵerror[:, i], lw = 4, label = label[i], markershape = :x, markersize = 10)
     end
     plt_ϵ_error
 end
 
+##############################################
 
-vecNmesh = 2 .^(3:8)
+#=
+# General Discretization Parameters
+T = Double64
+Rmin = zero(T)
+vecNmesh = 2 .^(3:7)
+=#
 
 #=
 plt_eigenvalue = plot_eigenvalue(100, 10, [2,3])
@@ -209,5 +212,6 @@ savefig(plt_eigenvector, "test/image_tests/Laplacien - Vecteur Propre 200 - ordr
 
 plt_eigenvalue_error = compute_error_eigenvalue(vecNmesh, 10, 1)
 savefig(plt_eigenvalue_error, "test/image_tests/Laplacien - Erreurs valeurs propres")
+
+compute_error_eigenvalue(vecNmesh, 10, 1)
 =#
-compute_error_eigenvalue(vecNmesh, 10, 5)
