8a832cb3592237c5cb751defa0fe8c6ec6dc2bf4
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2024-07-18 10:26:32 +0200

    integrate new structure of rational fraction to the computation of mass matrix

--- src/finite element basis/shortbasis/utils_computations.jl
+++ src/finite element basis/shortbasis/utils_computations.jl
@@ -18,39 +18,19 @@ end
     sum
 end
 
-@inline function integration_monome_over_linear(k, A, B, a, b)
-    @assert !iszero(A) || !iszero(B)
-    @assert k ≥ 0
-    if iszero(B)
-        if k == 0
-            1/A * log(abs(b/a))
-        else
-            1/(A * k) * (b^k - a^k)
-        end
-    elseif iszero(A)
-        return 1/(B * (k+1)) * (b^(k+1) - a^(k+1))
+
+@inline function weight_scalar_product(p::LaurentPolynomial{TP}, q::LaurentPolynomial{TQ}, weight::RationalFraction{TW}, a::Real, b::Real) where {TP, TQ, TW}
+    if degmax(weight.denom) > 2
+        return integrate((p*q*weight.num)/weight.denom, a, b; geomfun = false)
     else
-        C = B/A
-        right =  b^(k+1)*pFq((1,k+1),(k+2,),-b/C)
-        left = a^(k+1)*pFq((1,k+1),(k+2,),-a/C)
-        return (right-left)/(A*(C*k+C))
+        return integrate((p*q*weight.num)/weight.denom, a, b; geomfun = true)
     end
 end
 
-@inline function weight_scalar_product(p::LaurentPolynomial{TP}, q::LaurentPolynomial{TQ}, weight::RationalFraction{TW}, a::Real, b::Real) where {TP, TQ, TW}
-    if degmax_denom(weight) == 1
-        val = weight_scalar_product(p, q, weight.ent, a, b)
-        num = p*q*weight.num
-        Q,R = diveucl(num, weight.denom)
-        if iszero(R)
-            return val += integrate(Q, a,b)
-        else
-            for k ∈ eachindex(num)
-                val += num[k] * integration_monome_over_linear(k, weight.denom[1], weight.denom[0], a, b)
-            end 
-        return val
+@inline function weight_scalar_product(p::LaurentPolynomial{TP}, q::LaurentPolynomial{TQ}, weight::SommeRationalFraction{TW}, a::Real, b::Real) where {TP, TQ, TW}
+    val = weight_scalar_product(p, q, weight.ent, a, b)
+    for i ∈ eachindex(weight)
+        val += weight_scalar_product(p, q, weight[i], a, b)
     end
-    else
-        return integrate(p*q*weight.num/weight.denom, a,b) + weight_scalar_product(p, q, weight.ent, a, b)
-    end
+    val
 end
