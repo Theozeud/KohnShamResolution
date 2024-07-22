7a1d940b947ed8c0fdf61c5bbafece46dc3ac810
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2024-07-22 13:44:54 +0200

    Update utils_computations.jl

--- src/finite element basis/shortbasis/utils_computations.jl
+++ src/finite element basis/shortbasis/utils_computations.jl
@@ -4,8 +4,22 @@
     LaurentPolynomial([c0, c1], 0, false, T(0))
 end
 
+@memoize function fast_monom_scalar_product(p::LaurentPolynomial, n::Int, a::Real, b::Real)
+    scalar_product(Monomial(n), p, a, b)
+end
+
 @memoize function fast_monom_scalar_product(p::LaurentPolynomial, q::LaurentPolynomial, n::Int, a::Real, b::Real)
     scalar_product(Monomial(n) * p, q, a, b)
+end
+
+@inline function weight_scalar_product(p::LaurentPolynomial{TP}, weight::LaurentPolynomial{TW}, a::Real, b::Real) where {TP, TW}
+    @assert degmin(weight) ≥ 0
+    NewT = promote_type(TP,TW)
+    sum = NewT(0)
+    for i ∈ eachindex(weight)
+        sum += weight[i] * fast_monom_scalar_product(p, i, a, b)
+    end
+    sum
 end
 
 @inline function weight_scalar_product(p::LaurentPolynomial{TP}, q::LaurentPolynomial{TQ}, weight::LaurentPolynomial{TW}, a::Real, b::Real) where {TP, TQ, TW}
@@ -18,7 +32,16 @@ end
     sum
 end
 
-@inline function weight_scalar_product(p::LaurentPolynomial{TP}, q::LaurentPolynomial{TQ}, weight::RationalFraction{TW}, a::Real, b::Real) where {TP, TQ, TW}
+
+@inline function weight_scalar_product(p::LaurentPolynomial, weight::RationalFraction, a::Real, b::Real)
+    if degmax(weight.denom) > 2
+        return integrate((p*weight.num)/weight.denom, a, b; geomfun = false, enforceNullDelta = false)
+    else
+        return integrate((p*weight.num)/weight.denom, a, b; geomfun = true, enforceNullDelta = true)
+    end
+end
+
+@inline function weight_scalar_product(p::LaurentPolynomial, q::LaurentPolynomial, weight::RationalFraction, a::Real, b::Real)
     if degmax(weight.denom) > 2
         return integrate((p*q*weight.num)/weight.denom, a, b; geomfun = false, enforceNullDelta = false)
     else
@@ -26,12 +49,25 @@ end
     end
 end
 
-@inline function weight_scalar_product(p::LaurentPolynomial{TP}, q::LaurentPolynomial{TQ}, weight::SommeRationalFraction{TW}, a::Real, b::Real) where {TP, TQ, TW}
+@inline function weight_scalar_product(p::LaurentPolynomial, weight::SommeRationalFraction, a::Real, b::Real) 
+    val = weight_scalar_product(p, weight.ent, a, b)
+    for i ∈ eachindex(weight)
+        val += weight_scalar_product(p, weight[i], a, b)
+    end
+    val
+end
+
+@inline function weight_scalar_product(p::LaurentPolynomial, q::LaurentPolynomial, weight::SommeRationalFraction, a::Real, b::Real) 
     val = weight_scalar_product(p, q, weight.ent, a, b)
     for i ∈ eachindex(weight)
         val += weight_scalar_product(p, q, weight[i], a, b)
     end
     val
+end
+
+@inline function weight_scalar_product(p::LaurentPolynomial{TP}, weight::Base.Callable, a::Real, b::Real) where TP
+    f(x) = weight(x) * p(x)
+    approximate_integral(f, (a,b); method = QuadGKJL(), reltol = eps(TP), abstol = eps(TP))
 end
 
 @inline function weight_scalar_product(p::LaurentPolynomial{TP}, q::LaurentPolynomial{TQ}, weight::Base.Callable, a::Real, b::Real) where {TP, TQ}
@@ -40,7 +76,3 @@ end
     approximate_integral(f, (a,b); method = QuadGKJL(), reltol = eps(NewT), abstol = eps(NewT))
 end
 
-@inline function weight_scalar_product(p::LaurentPolynomial{TP}, weight::Base.Callable, a::Real, b::Real) where TP
-    f(x) = weight(x) * p(x)
-    approximate_integral(f, (a,b); method = QuadGKJL(), reltol = eps(TP), abstol = eps(TP))
-end
