a402228ae9d43426f315c2db7ee441192ac207d1
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2024-08-05 20:39:28 +0200

    Update oda.jl

--- src/methods/oda.jl
+++ src/methods/oda.jl
@@ -8,6 +8,7 @@ struct ConstantODA{T} <: ODA
     end
 end
 
+#= ODA not coded
 function update_density!(::ODA, solver::KhonShamSolver)
 
     @unpack Dprev = solver
@@ -17,13 +18,10 @@ function update_density!(::ODA, solver::KhonShamSolver)
     tmp_tn = 0.5
     tmp_D = tmp_tn * tmp_Dstar + (1 - tmp_tn) * Dprev
 end
+=#
 
 function update_density!(m::ConstantODA, solver::KhonShamSolver)
-
-    @unpack Dprev = solver
-    @unpack tmp_D, tmp_Dstar, tmp_U, tmp_n = solver.discretization.cache
-
-    build_density!(solver.discretization)
-    
-    tmp_D = m.t * tmp_Dstar + (1 - m.t) * Dprev
+    @unpack tmp_D, tmp_Dstar = solver.discretization.cache
+    tmp_D .= m.t * tmp_Dstar + (1 - m.t) * solver.Dprev
+    nothing
 end
