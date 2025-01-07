4ef118412dd1fb5917d69edef1bd596d672f6009
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2025-01-07 11:21:04 +0100

    Update solver.jl

--- src/solver.jl
+++ src/solver.jl
@@ -1,27 +1,25 @@
 struct SolverOptions{T}
-    ε::T            # Alg tolerance
+    scftol::T       # SCF tolerance
     maxiter::Int    # Maximum of iteration done
     quad_method     # Method to use for quadrature of integrals
     quad_reltol::T  # Relativ tolerance for the quadrature of integrals
     quad_abstol::T  # Absolute tolerance for the quadrature of integrals
-    hartree::T      # coefficient multiply to the Hartree Matrix : 0 -> no hartree term, 1-> full hartree term
+    hartree::T      # Coefficient multiply to the Hartree Matrix : 0 -> no hartree term, 1-> full hartree term
     degen_tol::T    
-    light::Bool
 end
 
-mutable struct KhonShamSolver{TSC}
-    discretization
-    model
-    method
-    D
-    Dprev
-    U  
-    ϵ
-    n
-    ϵhisto
-    Energyhisto
-    niter::Int
-    values_stop_crit::Vector{TSC}
-    current_stop_crit::TSC
-    opts::SolverOptions
+mutable struct KhonShamSolver
+    niter                           # Number of iterations
+    stop_criteria                   # Current stopping criteria
+    discretization                  # Discretization parameters
+    model                           # Model
+    method                          # Iterative method
+    opts                            # Solver options
+    D                               # Density Matrix at current time
+    Dprev                           # Density Matrix at previous time
+    U                               # Coefficient of orbitals at current time
+    ϵ                               # Orbitals energy at current time
+    n                               # Occupation number at current time 
+    energy                          # Total energy at current time
+    logbook                         # LogBook
 end
