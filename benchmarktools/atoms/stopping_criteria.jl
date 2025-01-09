01abed67be497802f6f947236f3447fc316e1aa1
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2025-01-09 18:01:15 +0100

    Update stopping_criteria.jl

--- benchmarktools/atoms/stopping_criteria.jl
+++ benchmarktools/atoms/stopping_criteria.jl
@@ -1,22 +1,22 @@
-function plot_stopping_criteria(sols, tol; title, label)
-    # Plot Criteria on Density
-    plt_criteria = plot(size = (650,500), margin = 0.5Plots.cm, legend = :outertopright, yaxis=:log,
-                        legendfontsize  = 12,  
-                        titlefontsize   = 12,
-                        guidefontsize   = 12,
-                        tickfontsize    = 12)
-    xlabel!(plt_criteria, "Iteration")
-    ylabel!(plt_criteria, "Convergence criteria")
-    title!(plt_criteria, title)
+function plot_stopping_criteria(sols; label = [""])
+    plt = plot( size = (650,500), margin = 0.5Plots.cm, legend = :outertopright, yaxis=:log,
+                legendfontsize  = 12,  
+                titlefontsize   = 12,
+                guidefontsize   = 12,
+                tickfontsize    = 12)
+    xlabel!(plt, "Iteration")
+    ylabel!(plt, "Scf Tolerance")
+    title!(plt, "Scf Tolerance through iterations")
     lmax = 0
-    for (i,sol) ∈ enumerate(sols)
-        plot!(plt_criteria, sol.crit, lw = 4, label = label[i], markershape = :x, markersize = 10)
-        if length(sol.crit)>lmax
-            lmax = length(sol.crit)
+    for i ∈ eachindex(sols)
+        @unpack stopping_criteria_log = sols[i].log
+        plot!(plt, stopping_criteria_log, lw = 4, label = label[i], markershape = :x, markersize = 10)
+        if length(stopping_criteria_log) > lmax
+            lmax = length(stopping_criteria_log)
         end
     end
-    plot!(ones(lmax)*tol, label = "tolerance", ls = :dash, color = :black, lw =4 )
-
+    #plot!(ones(lmax)*sols[1]., label = "tolerance", ls = :dash, color = :black, lw =4 )
+    plt
 end
 
 #=
