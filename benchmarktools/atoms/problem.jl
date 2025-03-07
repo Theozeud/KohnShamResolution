e7690ba03ffcfd22aeb94d56aa1eea668a480df1
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2025-03-07 10:40:05 +0100

    Update problem.jl

--- benchmarktools/atoms/problem.jl
+++ benchmarktools/atoms/problem.jl
@@ -1,7 +1,8 @@
 # STRUCTURE ATOM PROBLEM
 struct AtomProblem
     T           # Type of numbers
-    lh          # Truncation of orbital
+    lh          # Truncation of orbital for l
+    nh          # Truncation of orbital for n
     method      # SCF Method
     model       # Model
     Rmax        # Spatial cut-off
@@ -14,21 +15,21 @@ struct AtomProblem
     name        # Name of the problem
     solveropts  # Option for the solvers
 
-    AtomProblem(;T, lh, method, model, Rmax, Nmesh,typemesh, optsmesh, typebasis, optsbasis, typediscre, name = "", kwargs...) = 
-        new(T, lh, method, model, Rmax, Nmesh,typemesh, optsmesh, typebasis, optsbasis, typediscre, name, kwargs)
+    AtomProblem(;T, lh, nh, method, model, Rmax, Nmesh,typemesh, optsmesh, typebasis, optsbasis, typediscre, name = "", kwargs...) = 
+        new(T, lh, nh, method, model, Rmax, Nmesh,typemesh, optsmesh, typebasis, optsbasis, typediscre, name, kwargs)
 
     function AtomProblem(prob; 
-        T = prob.T, lh = prob.lh, method = prob.method, model = prob.model, Rmax = prob.Rmax, Nmesh = prob.Nmesh,
+        T = prob.T, lh = prob.lh, nh = prob.nh, method = prob.method, model = prob.model, Rmax = prob.Rmax, Nmesh = prob.Nmesh,
         typemesh = prob.typemesh, optsmesh = prob.optsmesh, typebasis = prob.typebasis, 
         optsbasis = prob.optsbasis, typediscre = prob.typediscre, name = prob.name, kwargs = prob.solveropts) 
-        new(T, lh, method, model, Rmax, Nmesh,typemesh, optsmesh, typebasis, optsbasis, typediscre, name, kwargs)
+        new(T, lh, nh, method, model, Rmax, Nmesh,typemesh, optsmesh, typebasis, optsbasis, typediscre, name, kwargs)
     end
 end
 
 function KohnShamResolution.groundstate(prob::AtomProblem)
     mesh = prob.typemesh(zero(prob.T), prob.Rmax, prob.Nmesh; T = prob.T, prob.optsmesh...)
     basis = prob.typebasis(mesh, prob.T; prob.optsbasis...)
-    discretization = prob.typediscre(prob.lh, basis, mesh)
+    discretization = prob.typediscre(prob.lh, basis, mesh, prob.nh)
     groundstate(prob.model, discretization, prob.method; name = prob.name, prob.solveropts...)
 end
 
