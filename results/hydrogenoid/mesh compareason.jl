2bdfc3ded828f074ffdc1584c6faa5b62e889603
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2025-03-24 20:25:12 +0100

    Expmesh is largely better

--- results/hydrogenoid/mesh compareason.jl
+++ results/hydrogenoid/mesh compareason.jl
@@ -9,10 +9,10 @@ const problinmesh = HydrogenoidProblem(;
                             l             = 0, 
                             Rmax          = 90, 
                             Nmesh         = 70,
-                            typemesh      = geometricmesh, 
-                            typebasis     = ShortP1IntLegendreBasis, 
-                            optsmesh      = (s=0.9,),  
-                            optsbasis     = (ordermax = 5, ),                           
+                            typemesh      = linmesh, 
+                            typebasis     = P1IntLegendreGenerator, 
+                            optsmesh      = (),  
+                            optsbasis     = (ordermax = 2, ),                           
                             name          = "IntLeg2-linmesh",
                             nU            = nothing)
 
@@ -25,14 +25,27 @@ const probgeomesh = HydrogenoidProblem(;
                             typemesh      = geometricmesh, 
                             typebasis     = P1IntLegendreGenerator, 
                             optsmesh      = (s = 0.9,),  
-                            optsbasis     = (ordermax = 5, ),                           
+                            optsbasis     = (ordermax = 2, ),                           
                             name          = "IntLeg2-geomesh",
+                            nU            = nothing)
+
+const probexpmesh = HydrogenoidProblem(; 
+                            T             = Float64, 
+                            z             = 1, 
+                            l             = 0, 
+                            Rmax          = 90, 
+                            Nmesh         = 70,
+                            typemesh      = expmesh, 
+                            typebasis     = P1IntLegendreGenerator, 
+                            optsmesh      = (s = 2.0,),  
+                            optsbasis     = (ordermax = 2, ),                           
+                            name          = "IntLeg2-expmesh",
                             nU            = nothing)
 #@time λgeo = eigvals_hydro(problinmesh)
 
 #@time sol = eigen_hydro(problinmesh)
 ##plt = plot_eigenvector(1, Ugeo, probgeomesh)
 
-convNmesh = convergenceNmesh(2 .^(3:8), [problinmesh, probgeomesh]; nums = [1])
+convNmesh = convergenceNmesh(2 .^(3:10), [problinmesh, probgeomesh, probexpmesh]; nums = [1])
 
 convergence_plot_Nmesh(convNmesh)
