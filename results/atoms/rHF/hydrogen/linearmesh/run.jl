48d6909a4cf2de64c9e9c62461685ff3c9791e97
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2025-03-25 20:13:00 +0100

    Update run.jl

--- results/atoms/rHF/hydrogen/linearmesh/run.jl
+++ results/atoms/rHF/hydrogen/linearmesh/run.jl
@@ -6,24 +6,24 @@ logconfig = LogConfig(orbitals_energy = true, occupation_number = true, energy =
 
 problem = AtomProblem(;
                 T               = Float64, 
-                lh              = 0,
-                nh              = 2, 
+                lh              = 1,
+                nh              = 3, 
                 method          = ODA(0.8), 
-                model           = ReducedHartreeFock(1, 1), 
+                model           = ReducedHartreeFock(8, 8), 
                 Rmax            = 80, 
                 Nmesh           = 80,
                 typemesh        = geometricmesh, 
                 optsmesh        = (s = 0.9,), 
                 typebasis       = P1IntLegendreGenerator, 
-                optsbasis       = (ordermax = 3,), 
+                optsbasis       = (ordermax = 5,), 
                 typediscre      = LDADiscretization,
                 name            = "test", 
-                scftol          = 1e-12,
-                maxiter         = 40,
+                scftol          = 1e-10,
+                maxiter         = 80,
                 hartree         = true,
                 degen_tol       = 1e-10,
                 logconfig       = logconfig,
-                verbose         = 0)
+                verbose         = 1)
 
 
 # RESOLUTION
