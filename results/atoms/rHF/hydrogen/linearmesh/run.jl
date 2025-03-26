caee2167c657ec74e5c7810dc1cf0f69da6e5888
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2025-03-26 13:08:24 +0100

    results

--- results/atoms/rHF/hydrogen/linearmesh/run.jl
+++ results/atoms/rHF/hydrogen/linearmesh/run.jl
@@ -6,24 +6,24 @@ logconfig = LogConfig(orbitals_energy = true, occupation_number = true, energy =
 
 problem = AtomProblem(;
                 T               = Float64, 
-                lh              = 0,
-                nh              = 2, 
+                lh              = 3,
+                nh              = 5, 
                 method          = ODA(0.8), 
-                model           = ReducedHartreeFock(2, 2), 
-                Rmax            = 100, 
-                Nmesh           = 40,
+                model           = ReducedHartreeFock(21, 21), 
+                Rmax            = 300, 
+                Nmesh           = 30,
                 typemesh        = expmesh, 
-                optsmesh        = (s = 2.0,), 
+                optsmesh        = (s = 1.5,), 
                 typebasis       = P1IntLegendreGenerator, 
-                optsbasis       = (ordermax = 10,), 
+                optsbasis       = (ordermax = 15,), 
                 typediscre      = LDADiscretization,
                 name            = "test", 
                 scftol          = 1e-9,
-                maxiter         = 40,
+                maxiter         = 100,
                 hartree         = true,
-                degen_tol       = 1e-10,
+                degen_tol       = 1e-3,
                 logconfig       = logconfig,
-                verbose         = 3)
+                verbose         = 1)
 
 
 # RESOLUTION
--- results/hydrogenoid/mesh compareason.jl
+++ results/hydrogenoid/mesh compareason.jl
@@ -6,39 +6,39 @@ include("../../benchmarktools/hydrogenoid/setup.jl")
 const problinmesh = HydrogenoidProblem(; 
                             T             = Float64, 
                             z             = 1, 
-                            l             = 0, 
-                            Rmax          = 90, 
+                            l             = 1, 
+                            Rmax          = 500, 
                             Nmesh         = 70,
                             typemesh      = linmesh, 
                             typebasis     = P1IntLegendreGenerator, 
                             optsmesh      = (),  
-                            optsbasis     = (ordermax = 2, ),                           
+                            optsbasis     = (ordermax = 4, ),                           
                             name          = "IntLeg2-linmesh",
                             nU            = nothing)
 
 const probgeomesh = HydrogenoidProblem(; 
                             T             = Float64, 
                             z             = 1, 
-                            l             = 0, 
-                            Rmax          = 90, 
+                            l             = 1, 
+                            Rmax          = 500, 
                             Nmesh         = 70,
                             typemesh      = geometricmesh, 
                             typebasis     = P1IntLegendreGenerator, 
                             optsmesh      = (s = 0.9,),  
-                            optsbasis     = (ordermax = 2, ),                           
+                            optsbasis     = (ordermax = 4, ),                           
                             name          = "IntLeg2-geomesh",
                             nU            = nothing)
 
 const probexpmesh = HydrogenoidProblem(; 
                             T             = Float64, 
                             z             = 1, 
-                            l             = 0, 
-                            Rmax          = 90, 
+                            l             = 2, 
+                            Rmax          = 500, 
                             Nmesh         = 70,
                             typemesh      = expmesh, 
                             typebasis     = P1IntLegendreGenerator, 
-                            optsmesh      = (s = 2.0,),  
-                            optsbasis     = (ordermax = 2, ),                           
+                            optsmesh      = (s = 1.0,),  
+                            optsbasis     = (ordermax = 4, ),                           
                             name          = "IntLeg2-expmesh",
                             nU            = nothing)
 #@time λgeo = eigvals_hydro(problinmesh)
@@ -46,6 +46,6 @@ const probexpmesh = HydrogenoidProblem(;
 #@time sol = eigen_hydro(problinmesh)
 ##plt = plot_eigenvector(1, Ugeo, probgeomesh)
 
-convNmesh = convergenceNmesh(2 .^(3:10), [problinmesh, probgeomesh, probexpmesh]; nums = [1])
+convNmesh = convergenceNmesh(2 .^(3:8), [problinmesh, probgeomesh, probexpmesh]; nums = [6])
 
 convergence_plot_Nmesh(convNmesh)
